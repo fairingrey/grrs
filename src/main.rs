2bdbc695dbdb8c6102999f6c00c75b6e06c1aadb
grey <fairingrey@gmail.com>
2019-03-18 15:32:12 -0700

    finish up, add test

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,11 @@
-use std::io::prelude::*;
-use std::io::BufReader;
 use std::fs::File;
+use std::io::BufReader;
+use std::io::Lines;
+use std::io::{self, prelude::*};
 use structopt::StructOpt;
+
+use exitfailure::ExitFailure;
+use failure::ResultExt;
 
 /// Search for a pattern in a file and display the lines that contain it.
 #[derive(StructOpt)]
@@ -13,19 +17,36 @@ struct Cli {
     path: std::path::PathBuf,
 }
 
-fn main() -> Result<(), Box<dyn std::error::Error>> {
+fn main() -> Result<(), ExitFailure> {
     let args = Cli::from_args();
 
-    let f = File::open(&args.path)?;
+    let f = File::open(&args.path)
+        .with_context(|_| format!("could not read file `{:?}`", &args.path))?;
     let reader = BufReader::new(f);
     let lines = reader.lines();
 
-    for line in lines {
-        let line = line?;
-        if line.contains(&args.pattern) {
-            println!("{}", line);
-        }
-    }
+    let stdout = io::stdout();
+    let handle = stdout.lock();
+
+    find_matches(lines, &args.pattern, handle)?;
 
     Ok(())
+}
+
+fn find_matches<B: BufRead + Sized>(lines: Lines<B>, pattern: &str, mut handle: impl Write) -> Result<(), ExitFailure> {
+    for line in lines {
+        let line = line?;
+        if line.contains(pattern) {
+            writeln!(handle, "{}", line)?;
+        }
+    }
+    Ok(())
+}
+
+#[test]
+fn find_a_match() {
+    let mut result = Vec::new();
+    let lines = b"lorem ipsum\ndolor sit amet".lines();
+    let _ = find_matches(lines, "lorem", &mut result);
+    assert_eq!(result, b"lorem ipsum\n");
 }
